5ebb4f5e5aea560d0fffd04890a480952ffc4681
luojian <luojian0806@gmail.com>
2019-11-12 22:35:35 +0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    Center(
+    App()
+  );
+}
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
       child: Text(
         "Hello",
         textDirection: TextDirection.ltr,
       ),
-    )
-  );
+    );
+  }
 }
